f85050f9a40ea5668b0250d7cea46773950b9bcf
criess374 <christoph.riess@empa.ch>
2026-04-09 10:01:04 +0000

    change tests

--- tests/test_length.py
+++ tests/test_length.py
@@ -1,7 +1,7 @@
 from mypackage.geometry import Point as Point, Line as Line
 from mypackage.utils import distance as distance
 
-def test_distance():
+def test_length():
     p1 = Point(1,1)
     p2 = Point(4,6)
     l1 = Line(p1,p2)
